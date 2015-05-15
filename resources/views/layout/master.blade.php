898e0173146d9587da27ee53da81e658109086d3
Joe Cianflone <joe@cianflone.co>
2015-05-14 23:03:21 -0400

    minor update

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -4,10 +4,10 @@
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <title>@yield("title")</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta name="description" content="@yield('description')">
 
-      <!-- Google+ because that one asshat in your company is gonna ask about it -->
+      <!-- Google+ because that one guy in your company is gonna ask about it -->
       <meta itemprop="name" content="@yield('gplus-name')">
       <meta itemprop="description" content="@yield('gplus-description')">
       <meta itemprop="image" content="@yield('gplus-image')">
