be8e13cd32d24160151931938eb14112ce14cca9
Joe Cianflone <joe@cianflone.co>
2015-05-18 10:47:24 -0400

    links and metadata

--- resources/views/fragments/downloads.blade.php
+++ resources/views/fragments/downloads.blade.php
@@ -1,6 +1,6 @@
 <h2 class="downloads__header">Download for FREE!</h2>
 <ul class="downloads__badges">
-   <li class="downloads__badges--ios"><a href="#" title=""><img src="/assets/images/badge-ios.svg" alt=""></a></li>
-   <li class="downloads__badges--android"><a href="#" title=""><img src="/assets/images/badge-google.svg" alt=""></a></li>
+   <li class="downloads__badges--ios"><a href="https://itunes.apple.com/us/app/aupeo!-radio/id335966307?mt=8" title="iOS"><img src="/assets/images/badge-ios.svg" alt=""></a></li>
+   <li class="downloads__badges--android"><a href="https://play.google.com/store/apps/details?id=com.aupeo.AupeoNextGen&amp;hl=en" title="Google Play"><img src="/assets/images/badge-google.svg" alt=""></a></li>
 </ul>
 <span class="arrow--point-up fa fa-caret-up"></span>
--- resources/views/fragments/footer.blade.php
+++ resources/views/fragments/footer.blade.php
@@ -1,3 +1,3 @@
 <p>
-   &copy; AUPEO! <span class="js-year">2015</span> <a href="#" title="">Terms of Service</a> <span class="breaker">&#124;</span> <a href="#" title="">Privacy Policy</a>
+   &copy; AUPEO! <span class="js-year">2015</span> <a href="http://s3.amazonaws.com/users-production/agreements/contents/000/000/008/original/2014-08-26_AUPEO_Terms_of_Use_(German_Law).htm?1409142516" title="Terms of Service">Terms of Service</a> <span class="breaker">&#124;</span> <a href="http://s3.amazonaws.com/users-production/agreements/contents/000/000/006/original/AUPEO_-_German_Privacy_Policy.htm?1409142252" title="Privacy Policy">Privacy Policy</a>
 </p>
