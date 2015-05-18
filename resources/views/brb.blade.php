ac40835c43b49ee0d269e7bded247b7dda5f60a1
Joe Cianflone <joe@cianflone.co>
2015-05-18 12:05:38 -0400

    addroll and brb page

---
+++ resources/views/brb.blade.php
@@ -0,0 +1,24 @@
+@extends('layout.master')
+@section('title',             'Personal Radio by AUPEO!&reg;')
+@section('description',       'Personal Radio by AUPEO!&reg; provides you with music and other audio content (in the US only) which reflects your tastes and interests on your smartphone, connected devices and in select vehicles')
+
+@section('gplus-title',       'Personal Radio by AUPEO!&reg;')
+@section('gplus-image',       '/assets/images/logo.svg')
+@section('gplus-description', 'Personal Radio by AUPEO!&reg; provides you with music and other audio content (in the US only) which reflects your tastes and interests on your smartphone, connected devices and in select vehicles')
+
+@section('og-type',           'website')
+@section('og-title',          'Personal Radio by AUPEO!&reg;')
+@section('og-image',          '/assets/images/logo.svg')
+@section('og-sitename',       'Personal Radio by AUPEO!&reg; Premium Trial')
+@section('og-description',    'Personal Radio by AUPEO!&reg; provides you with music and other audio content (in the US only) which reflects your tastes and interests on your smartphone, connected devices and in select vehicles')
+
+@section('t-url',             'http://aupeopremiumtrial.com')
+@section('t-title',           'Personal Radio by AUPEO!&reg;')
+@section('t-image',           '/assets/images/logo.svg')
+@section('t-description',     'Personal Radio by AUPEO!&reg; provides you with music and other audio content (in the US only) which reflects your tastes and interests on your smartphone, connected devices and in select vehicles')
+
+@section('content')
+   <div class="container">
+      <h1 style="padding: 20px 0; font-size: 3.2em; color: #fff; text-align: center;">We're updating the site and we'll be right back.</h1>
+   </div>
+@stop
--- resources/views/fragments/scripts.blade.php
+++ resources/views/fragments/scripts.blade.php
@@ -1,11 +1,31 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="/assets/js/jquery.min.js"><\/script>')</script>
 <script src="/assets/js/app.min.js"></script>
+<script type="text/javascript">
+adroll_adv_id = "MPS7LHIVXJGWJAZNUTTV26";
+adroll_pix_id = "4R3HXQ32CVFV3ASQ6PYJRF";
+(function () {
+var oldonload = window.onload;
+window.onload = function(){
+   __adroll_loaded=true;
+   var scr = document.createElement("script");
+   var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
+   scr.setAttribute('async', 'true');
+   scr.type = "text/javascript";
+   scr.src = host + "/j/roundtrip.js";
+   ((document.getElementsByTagName('head') || [null])[0] ||
+    document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
+   if(oldonload){oldonload()}};
+}());
+</script>
+
 <script>
-    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-    e.src='https://www.google-analytics.com/analytics.js';
-    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-    ga('create','','auto');ga('send','pageview');
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-47878724-10', 'auto');
+  ga('send', 'pageview');
+
 </script>
